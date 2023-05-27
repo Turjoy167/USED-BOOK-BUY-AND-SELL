995806e38a9881d3b2f059550c98a34e2cc04f7b
Turjoy167 <49525843+Turjoy167@users.noreply.github.com>
2023-05-28 03:46:23 +0600

    Add files via upload

---
+++ application/views/temp/contact_modal.php
@@ -0,0 +1,31 @@
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Contact Information</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div><p>You can easily contact with our support team. They are waiting to support you. You can ask any types of query about this application and our policy. You can also contact if you face any types of shipping problem.</p></div>
+        <div id="social-icon">
+            <h6>Leave us a messege here</h6>
+            <span><a href="https://www.facebook.com/turjoybanik.tlc/" title="Facebook" target="_blank"><i class="fab fa-facebook-f"></i></a></span>
+            <span><a href="https://github.com/Turjoy167" title="Github" target="_blank"><i class="fab fa-github"></i></a></span>
+            <span><a href="https://www.instagram.com/turjoy_banik/" title="Instagram" target="_blank"><i class="fab fa-instagram"></i></a></span>
+        </div>
+        <br>
+        <div>
+            <h6>Thanks & Regards</h6>
+            <a href="#" class="text-info"><b>Plabon Banik</b></a>
+            <p>Founder of Bookshop and E-learning.</p>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
